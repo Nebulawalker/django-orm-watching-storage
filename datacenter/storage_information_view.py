650cbec3a6124757dcba0b4bbd01f52db60c2611
Roman Okatev <43115066+Nebulawalker@users.noreply.github.com>
2022-12-21 23:42:12 +0300

    Рефакторинг

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -2,16 +2,10 @@ from datacenter.models import Passcard
 from datacenter.models import Visit
 from django.shortcuts import render
 from django.utils.timezone import localtime
-import time
+from datacenter.format_duration import format_duration
 
-def format_duration(duration):
-    duration_in_seconds = time.gmtime(duration)
-    formatted_duration = time.strftime("%H ч %M мин", duration_in_seconds)
-    return formatted_duration
 
 def storage_information_view(request):
-    # Программируем здесь
-
     guards_in_storage = Visit.objects.filter(leaved_at=None)
     non_closed_visits = []
     for guard_in_storage in guards_in_storage:
@@ -27,6 +21,6 @@ def storage_information_view(request):
         )
 
     context = {
-        'non_closed_visits': non_closed_visits,  # не закрытые посещения
+        'non_closed_visits': non_closed_visits,  
     }
     return render(request, 'storage_information.html', context)
