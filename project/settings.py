a568cc8220c43d768726836aa1c9460f8796d6fd
Roman Okatev <43115066+Nebulawalker@users.noreply.github.com>
2023-01-11 23:36:16 +0300

    Добавлена переменная окружения, исправлен README.md

--- project/settings.py
+++ project/settings.py
@@ -17,13 +17,13 @@ DATABASES = {
 
 INSTALLED_APPS = ['datacenter']
 
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = env('SECRET_KEY', 'Replace me')
 
-DEBUG = env.bool('DEBUG', False)
+DEBUG = env.bool('DEBUG', True)
 
 ROOT_URLCONF = 'project.urls'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', ['localhost', '0.0.0.0'])
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
